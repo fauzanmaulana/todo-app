5c9fbd07b0d9db39ce67674743bab6164d921053
fauzanmaulana <fauzanfm735@gmail.com>
2020-07-17 17:30:50 +0700

    write list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { TextField, Button, List, ListItem, ListItemIcon, Checkbox, ListItemText, Container } from '@material-ui/core';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
 function App() {
+
+  const useStyles = makeStyles((item) => ({
+    btnmargin: {
+      margin: item.spacing(1)
+    }
+  }));
+
+  const classes = useStyles()
+
+  const [lists, setList] = useState([])
+  const [input, setInput] = useState('')
+  const [checked, setChecked] = useState([0])
+
+  const addList = e => {
+    e.preventDefault()
+    setList([...lists, input])
+    setInput('')
+  }
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="sm">
+
+      <h1>Write the things that you have todo bellow!</h1>
+      <form>
+        <TextField value={input} onChange={e => setInput(e.target.value)} id="list-input" label="write here.." variant="outlined" />
+
+        <Button onClick={addList} className={classes.btnmargin} variant="outlined" color="primary" size="large" type="submit">Add</Button>
+
+        <List>
+          {lists.map((value, index) => (
+            <ListItem key={index} role={undefined} dense onClick={handleToggle(value)}>
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                checked={checked.indexOf(value) !== -1}
+                tabIndex={-1}
+                disableRipple
+                inputProps={{ 'aria-labelledby': `checkbox-list-label-${value}` }}
+              />
+            </ListItemIcon>
+            <ListItemText id={`checkbox-list-label-${value}`} primary={value} />
+            <DeleteOutlinedIcon/>
+          </ListItem>
+          ))}
+        </List>
+        
+      </form>
+      </Container>
     </div>
   );
 }
